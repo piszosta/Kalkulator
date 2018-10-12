b53a71d8dacdf35b2fb1f2ee3100ae5c98bb1f18
piszosta <piotrszostak@gmail.com>
2018-10-12 02:15:11 +0200

    new class

--- src/main/java/com/calculator/kalkulator/KalkulatorApplication.java
+++ src/main/java/com/calculator/kalkulator/KalkulatorApplication.java
@@ -4,9 +4,33 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+class Calc{
+    int numberA;
+    int numberB;
+    int sum;
+
+    public Calc (int numberA, int numberB){
+        this.numberA = numberA;
+        this.numberB = numberB;
+    }
+
+    public void adding(){
+        sum = numberA + numberB;
+    }
+
+    public int getSum(){
+        return sum;
+    }
+}
 public class KalkulatorApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(KalkulatorApplication.class, args);
-    }
+
+    Calc calculator = new Calc(1, 2);
+    calculator.adding();
+        int sum = calculator.getSum();
+    System.out.println("The sum is " + sum);
+}
+
 }
